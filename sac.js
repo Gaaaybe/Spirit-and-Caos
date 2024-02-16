383b4f7022adf3cf3e314491c84d3f3caf57cbfa
Gaaaybe <gabenesus@gmail.com>
2024-02-16 19:08:28 -0300

    Adiciona arquivos e faz alterações no sistema Espirito e Caos

--- sac.js
+++ sac.js
@@ -1,4 +1,11 @@
+import SACItemSheet from "./module/sheets/SACItemSheet.js";
+//import { SaC } from "./module/config.js";
 
 Hooks.once("init", function() {
-    console.log("sac | Iniciando sistema Espirito e Caos...")
+    console.log("SAC | Iniciando sistema Espirito e Caos...")
+
+//    CONFIG.SaC = SaC;
+
+    Items.unregisterSheet("core", ItemSheet)
+    Items.registerSheet("spirit-and-caos", SACItemSheet, {makeDefault: true})
 })
